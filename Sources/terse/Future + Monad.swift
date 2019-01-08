55f4c1cef6327fca38217b7a9c1e394ca0b4de4a
John Connolly <connoljo2@gmail.com>
2019-01-08 06:35:45 -0400

    Added pipe

--- Sources/terse/Future + Monad.swift
+++ Sources/terse/Future + Monad.swift
@@ -10,12 +10,12 @@ import NIO
 
 infix operator >>-: MonadicPrecedenceLeft
 
-/// flatmaps a Future if the future throws an error f with never be called.
 public func >>- <A, B>(a: EventLoopFuture<A>, f: @escaping (A) -> EventLoopFuture<B>) -> EventLoopFuture<B> {
     return a.then { value -> EventLoopFuture<B> in
         return f(value)
     }
 }
+
 
 infix operator -<<: MonadicPrecedenceRight
 
@@ -27,7 +27,6 @@ public func -<< <A, B>(f: @escaping (A) -> EventLoopFuture<B>, a: EventLoopFutur
 
 infix operator >=>: MonadicPrecedenceLeft
 
-/// left-to-right composition operator.
 public func >=> <A, B, C>(f: @escaping (A) -> EventLoopFuture<B>,
                           g: @escaping (B) -> EventLoopFuture<C>) -> (A) -> EventLoopFuture<C> {
     return { a in
@@ -42,4 +41,55 @@ public func <=< <A, B, C>(f: @escaping (B) -> EventLoopFuture<C>,
     return { a in
         g(a) >>- f
     }
+}
+
+
+public func pipe<A, B, C>(
+    _ f: @escaping (_ a: A) -> EventLoopFuture<B>,
+    _ g: @escaping (_ b: B) -> EventLoopFuture<C>
+    )
+    -> (A) -> EventLoopFuture<C> {
+    return f >=> g
+}
+
+public func pipe<A, B, C, D>(
+    _ f: @escaping (A) -> EventLoopFuture<B>,
+    _ g: @escaping (B) -> EventLoopFuture<C>,
+    _ h: @escaping (C) -> EventLoopFuture<D>
+    )
+    -> (A) -> EventLoopFuture<D> {
+    return f >=> g >=> h
+}
+
+public func pipe<A, B, C, D, E>(
+    _ f: @escaping (A) -> EventLoopFuture<B>,
+    _ g: @escaping (B) -> EventLoopFuture<C>,
+    _ h: @escaping (C) -> EventLoopFuture<D>,
+    _ i: @escaping (D) -> EventLoopFuture<E>
+    )
+    -> (A) -> EventLoopFuture<E> {
+    return f >=> g >=> h >=> i
+}
+
+public func pipe<A, B, C, D, E, F>(
+    _ f: @escaping (A) -> EventLoopFuture<B>,
+    _ g: @escaping (B) -> EventLoopFuture<C>,
+    _ h: @escaping (C) -> EventLoopFuture<D>,
+    _ i: @escaping (D) -> EventLoopFuture<E>,
+    _ j: @escaping (E) -> EventLoopFuture<F>
+    )
+    -> (A) -> EventLoopFuture<F> {
+    return f >=> g >=> h >=> i >=> j
+}
+
+public func pipe<A, B, C, D, E, F, G>(
+    _ f: @escaping (A) -> EventLoopFuture<B>,
+    _ g: @escaping (B) -> EventLoopFuture<C>,
+    _ h: @escaping (C) -> EventLoopFuture<D>,
+    _ i: @escaping (D) -> EventLoopFuture<E>,
+    _ j: @escaping (E) -> EventLoopFuture<F>,
+    _ k: @escaping (F) -> EventLoopFuture<G>
+    )
+    -> (A) -> EventLoopFuture<G> {
+    return f >=> g >=> h >=> i >=> j >=> k
 }
